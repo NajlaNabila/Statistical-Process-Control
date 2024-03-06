8fa474a2dde1bcc8f88d41bf0110875066114c0f
Nur Najla Nabila bt Azman <154044136+NajlaNabila@users.noreply.github.com>
2024-03-06 19:35:44 +0800

    Update chap6program.R

--- chap6program.R
+++ chap6program.R
@@ -5,18 +5,29 @@
 attach(HardBakeData)
 
 library(dplyr)
+library(qcc)
 
-a1=HardBakeData %>%
-  group_by(Sample.Number) %>%
+# xbarbar
+a1=HardBakeData |>
+  group_by(Sample.Number) |>
   summarise_at(vars(Flow.Width), list(mean = mean))
- 
+
 grand.mean=mean(a1$mean)
 
+# R bar
 a2=HardBakeData %>%
   group_by(Sample.Number) %>%
   summarise_at(vars(Flow.Width), list(max=max,min=min))
 
-range.bake=aa$max-aa$min
+range.bake=a2$max-a2$min
 
 R.mean=mean(range.bake)
+
+#xbar chart model
+A2=0.577
+lcl = grand.mean-A2*R.mean
+UCL=grand.mean+A2*R.mean
+
+#xbar chart plot
+qcc(data=a1$mean,stdev=0.32521,limits=c(lcl,UCL),plot=TRUE)
 
